3c68a33b2171d801c4845c9c386b2643cb0f5497
Shachar <shachar147@gmail.com>
2025-07-05 23:46:07 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Load environment variables
 dotenv.config();
@@ -8,6 +9,16 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('BuySearch API')
+    .setDescription('API documentation for BuySearch v2')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
