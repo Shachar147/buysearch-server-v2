c4baba71956560d445646ac713d5654973c90572
Shachar <shachar147@gmail.com>
2025-07-08 12:02:00 +0300

    add cron job

--- src/scrapers/asos-scraper-cron.service.ts
+++ src/scrapers/asos-scraper-cron.service.ts
@@ -3,15 +3,19 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { exec } from 'child_process';
 import { join } from 'path';
 
+const CronexpressionExtended = {
+  TWICE_DAILY: '0 2,12 * * *'
+}
+
 @Injectable()
 export class AsosScraperCronService {
   private readonly logger = new Logger(AsosScraperCronService.name);
 
-  @Cron(CronExpression.EVERY_DAY_AT_NOON)
+  @Cron(CronexpressionExtended.TWICE_DAILY)
   async handleCron() {
     this.logger.log('Running ASOS scraper cron job...');
-    const scraperPath = join(__dirname, 'asos_scraper.ts');
-    exec('npx ts-node ' + scraperPath, { cwd: process.cwd() }, (error, stdout, stderr) => {
+    const scraperPath = join(__dirname, 'asos_scraper.js');
+    exec('node ' + scraperPath, { cwd: process.cwd() }, (error, stdout, stderr) => {
       if (error) {
         this.logger.error(`Error: ${error.message}`);
         return;
